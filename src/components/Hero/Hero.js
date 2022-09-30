2398c298bae67991ac3ca905efede11ad20e0b43
Alexeii <alexeii.yatsenko@gmail.com>
2022-10-01 00:55:52 +0300

    save state

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,8 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <div>
-      <Card>
+    <div className={styles.hero}>
+      <Card className={styles['card-customize']}>
         <h3>Lorem ipsum</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio dolor dolore ducimus excepturi harum magnam maiores modi, nesciunt nulla officia placeat quam quis, rem repudiandae vitae voluptatem voluptates, voluptatibus.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi deserunt ducimus eos impedit incidunt itaque tempora temporibus velit voluptas. Blanditiis fugit illo provident quasi sapiente. Doloribus natus officiis voluptatibus!</p>
